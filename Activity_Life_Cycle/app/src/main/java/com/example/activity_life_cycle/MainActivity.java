297d81cae98b452589d59e9b1aa3ed6ac7bf50ea
prajwal_gade30 <prajwalgade30@gmail.com>
2026-06-29 13:42:31 +0530

    life cycle

---
+++ Activity_Life_Cycle/app/src/main/java/com/example/activity_life_cycle/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.activity_life_cycle;
+
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "ActivityLifeCycle";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate() called");
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d(TAG, "onStart() called");
+        Toast.makeText(this, "onStart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "onResume() called");
+        Toast.makeText(this, "onResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "onPause() called");
+        Toast.makeText(this, "onPause()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(TAG, "onStop() called");
+        Toast.makeText(this, "onStop()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.d(TAG, "onRestart() called");
+        Toast.makeText(this, "onRestart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG, "onDestroy() called");
+        Toast.makeText(this, "onDestroy()", Toast.LENGTH_SHORT).show();
+    }
+}
